363789becdb7be2a19c77c1c26819f7b61e5aec2
majid111 <abdullahalmajid@gmail.com>
2024-01-26 07:36:43 +0600

    added section2 and news card

---
+++ firstjs.js
@@ -0,0 +1,19 @@
+
+var country = 'Bangladesh';
+var flo=5.5;
+var integ=1111;
+var ok = true;
+
+
+
+
+console.log(typeof ok)
+console.log(typeof flo)
+console.log (typeof integ)
+console.log (typeof 100)
+console.log(typeof country)
+console.log(ok)
+console.log(flo)
+console.log(integ)
+console.log(100)
+console.log(country)
